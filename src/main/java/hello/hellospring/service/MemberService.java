6d85eb1a7b0246437f07b067a5b035b532fe9f00
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-10-20 12:57:41 +0900

    서비스 패키지 작성

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+//    회원가입
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원은 안된다
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())//변수 추출 MacOS: Cmd + Opt + v  Win/Linux: Ctrl + Alt + v
+            .ifPresent(m -> {          //메서드 추출하기 (Extract -> Method) MacOS: Cmd + Opt + m Win/Linux: Ctrl + Alt + m
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+
+
+    //전체 회원 조회
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
