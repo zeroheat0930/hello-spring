20ed1eee4394494913b4fdd5727323de686f59f1
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-10-21 22:07:55 +0900

    회원 앱 기능 등록 MemberController에 postmapping추가 memberform creatememberform.html 작성후

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
@@ -15,4 +18,21 @@ public class MemberController {
     public MemberController(MemberService memberService) { //이렇게 생성자 주입이 깔끔
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public  String creatFrom() {
+        return "members/createMemberForm";
+    } //getmapping은 url창 엔터 딱치는거
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName()); //확인용
+
+        memberService.join(member);
+
+        return  "redirect:/";
+    } //데이타를 뭔가 폼같은데 넣어서 전달할때 postmapping
 }
