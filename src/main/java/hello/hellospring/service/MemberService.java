5e6289071658430f4e8c3ab4429ef4b03301bde1
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-10-22 22:35:25 +0900

    MemberService 회원 조회 시간 측정 추가
    AOP써야하는 환경 가정해서 문제만듬

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,10 +23,18 @@ public class MemberService {
 
     //    회원가입
     public Long join(Member member) {
+
+        long start = System.currentTimeMillis();
         //같은 이름이 있는 중복 회원은 안된다
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+                long finish = System.currentTimeMillis();
+                long timeMs = finish - start;
+                System.out.println("join " + timeMs + "ms");
+            }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -40,7 +48,14 @@ public class MemberService {
 
     //전체 회원 조회
     public List<Member> findMember(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
