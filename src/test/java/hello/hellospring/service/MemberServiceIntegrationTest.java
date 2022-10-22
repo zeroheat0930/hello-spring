c4e2a86ab2a0df4d54fd707a8b2d25cb569e397f
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-10-22 15:36:36 +0900

    jdbc template이라는 기능을 쓰면 ㅈㄹ 쉽게 리포지토리 작성 가능 그에따른 test도 쉬워짐 SpringConfig 에서 조립하면끝

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest //test 케이스할때 여기 2개 @붙여주면쉬워짐
@@ -21,33 +22,29 @@ class MemberServiceIntegrationTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
-    void 회원가입() {
-        //given
+    public void 회원가입() throws Exception {
+        //Given
         Member member = new Member();
-        member.setName("Hello");
-        
-        //when
+        member.setName("spring");
+        //When
         Long saveId = memberService.join(member);
-        
-        //then
-        Member findMember= memberService.findOne(saveId).get();
-        assertThat(member.getName()).isEqualTo(findMember.getName());
+        //Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
 
     }
     @Test
-    public void 중복_회원_예외(){
-        //given
+    public void 중복_회원_예외() throws Exception {
+        //Given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
-        //when
-
+        //When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
     }
 
 }
